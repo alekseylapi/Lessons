635a96061492d8c218a8e8b4887ed46852f31b8a
dgavrish <dgavrish@azbukavkusa.ru>
2025-03-19 20:31:34 +0300

    Lesson 6: Dog

--- app/index.php
+++ app/index.php
@@ -2,7 +2,5 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-//echo \Alekseylapi\Lessons\Lesson4\Main::foo(), PHP_EOL;
-
-var_dump(\Alekseylapi\Lessons\Lesson5\Main::task1(1, 1));
+\Alekseylapi\Lessons\Lesson6\Main::handle();
 
---
+++ app/src/Lesson6/Main.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace Alekseylapi\Lessons\Lesson6;
+
+class Main
+{
+    public static function handle()
+    {
+//        self::dogVariables();
+//        self::dogAssociativeArray();
+//        self::dogStruct();
+//        self::dogClass();
+        self::dogEncapsulation();
+    }
+
+    public static function dogVariables(): void
+    {
+        $dog1Name = 'Sharik';
+        $dog1Birthday = new \DateTimeImmutable("2020-03-19 00:00:00");
+        $dog1Breed = "Taxa";
+        $dog1Sex = "M";
+        $dog1Master = "Vasya";
+
+        $dog2Name = 'Tuzik';
+        $dog2Birthday = new \DateTimeImmutable("2023-03-19 00:00:00");
+        $dog2Breed = "Taxa";
+        $dog2Sex = "M";
+        $dog2Master = "Vasya";
+
+
+        echo $dog1Birthday->diff(new \DateTimeImmutable())->y, PHP_EOL;
+        echo $dog2Birthday->diff(new \DateTimeImmutable())->y, PHP_EOL;
+    }
+
+    public static function dogAssociativeArray(): void
+    {
+        $dogs = [];
+
+        $dog1 = [
+            'name' => 'Sharik',
+            'birthday' => new \DateTimeImmutable("2020-03-19 00:00:00"),
+            'breed' => "Taxa",
+            'sex' => "M",
+            'master' => "Vasya",
+        ];
+        $dogs[] = $dog1;
+
+        $dog2 = [
+            'name' => 'Tuzik',
+            'birthday' => new \DateTimeImmutable("2023-03-19 00:00:00"),
+            'breed' => "Taxa",
+            'sex' => "M",
+            'master' => "Vasya",
+        ];
+        $dogs[] = $dog2;
+
+        foreach ($dogs as $dog) {
+//            echo $dog['birthday']->diff(new \DateTime())->y, PHP_EOL;
+            echo self::getDogAge($dog), PHP_EOL;
+        }
+    }
+
+    public static function getDogAge(array $dog): int
+    {
+        return $dog['birthday']->diff(new \DateTime())->y;
+    }
+
+    public static function dogStruct(): void
+    {
+        $dogs = [];
+        $dog1 = new Dog(
+            'Sharik',
+            "2020-03-19",
+            "Taxa",
+            "M",
+            "Vasya",
+        );
+        $dogs[] = $dog1;
+
+        $dog2 = new Dog(
+            'Tuzik',
+            "2023-03-19",
+            "Taxa",
+            "M",
+            "Vasya",
+        );
+        $dogs[] = $dog2;
+
+        foreach ($dogs as $dog) {
+//            echo $dog->birthday->diff(new \DateTime())->y, PHP_EOL;
+            echo self::getDogAge2($dog), PHP_EOL;
+        }
+    }
+
+    public static function getDogAge2(Dog $dog): int
+    {
+        return $dog->getBirthday()->diff(new \DateTime())->y;
+    }
+
+    public static function dogClass(): void
+    {
+        $dogs = [];
+        $dog1 = new Dog(
+            'Sharik',
+            "2020-03-19",
+            "Taxa",
+            "M",
+            "Vasya",
+        );
+        $dogs[] = $dog1;
+
+        $dog2 = new Dog(
+            'Tuzik',
+            "2023-03-19",
+            "Taxa",
+            "M",
+            "Vasya",
+        );
+        $dogs[] = $dog2;
+
+        foreach ($dogs as $dog) {
+            echo $dog->name, ": ",
+                $dog->getAge(), ", ",
+                PHP_EOL;
+        }
+    }
+
+    public static function dogEncapsulation(): void
+    {
+        $dog1 = new Dog(
+            'Sharik',
+            "2020-03-19",
+            "Taxa",
+            "M",
+            "Vasya",
+        );
+
+        echo $dog1->getAge(), PHP_EOL;
+
+        echo $dog1->getBirthday()->format("d.m.Y"), PHP_EOL;
+
+        $birthday = $dog1->getBirthday();
+        $anotherBirthday = $birthday->add(new \DateInterval("P1Y"));
+//        $birthday = new \DateTimeImmutable("2022-12-22");
+        echo $anotherBirthday->format("d.m.Y"), PHP_EOL;
+
+        echo $dog1->getBirthday()->format("d.m.Y"), PHP_EOL;
+
+        $dog2 = new Dog(
+            'Tuzik',
+            "2023-03-19",
+            "Taxa",
+            "M",
+            "Vasya",
+        );
+
+        var_dump($dog1->isOlder($dog2));
+//        echo $dog1->somePrivateFunction(), PHP_EOL;
+    }
+}
