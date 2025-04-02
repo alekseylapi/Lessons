1d03b6113de790192107cd0c725dddce8e24f13b
lapin.aleksey.devs@gmail.com <NBAlive228@>
2025-04-02 20:54:05 +0300

    Lesson 7

--- app/index.php
+++ app/index.php
@@ -7,26 +7,21 @@ require __DIR__ . '/vendor/autoload.php';
 $master1 = \Alekseylapi\Lessons\Lesson7\HomeWork::buildMaster1();
 $master2 = \Alekseylapi\Lessons\Lesson7\HomeWork::buildMaster2();
 $masters = [$master1, $master2];
-$dog = $master1->pets[0];
+$dog = $master2->pets[0];
 
 //\Alekseylapi\Lessons\Lesson6\Main::handle();
 //Ale//echo \Alekseylapi\Lessons\Lesson4\Main::task13(54321);kseylapi\Lessons\Lesson7\Main::extending();
 //\Alekseylapi\Lessons\Lesson7\HomeWork::printDogsInfo($master1);
 //\Alekseylapi\Lessons\Lesson7\HomeWork::printDogsInfo($master2);
 //\Alekseylapi\Lessons\Lesson7\HomeWork::printMastersInfo([$master1]);
-//$foundMaster = \Alekseylapi\Lessons\Lesson7\HomeWork::findMasterByDog($dog, $masters);
-//if ($foundMaster) {
-//    echo "Хозяин собаки {$dog->name}: {$foundMaster->name}\n";
-//} else {
-//    echo "Хозяин для собаки {$dog->name} не найден.\n";
-//}
-//\Alekseylapi\Lessons\Lesson7\HomeWork::printOtherDogs($master2, $dog);
-echo "До передачи:\n";
-\Alekseylapi\Lessons\Lesson7\HomeWork::printDogsInfo($master1);
-\Alekseylapi\Lessons\Lesson7\HomeWork::printDogsInfo($master2);
-
-\Alekseylapi\Lessons\Lesson7\HomeWork::transferDog($dog, $master1, $master2);
-
-echo "\nПосле передачи:\n";
-\Alekseylapi\Lessons\Lesson7\HomeWork::printDogsInfo($master1);
-\Alekseylapi\Lessons\Lesson7\HomeWork::printDogsInfo($master2);
+//\Alekseylapi\Lessons\Lesson7\HomeWork::findMasterByDog($dog);
+\Alekseylapi\Lessons\Lesson7\HomeWork::printOtherDogs($dog);
+//echo "До передачи:\n";
+//\Alekseylapi\Lessons\Lesson7\HomeWork::printDogsInfo($master1);
+//\Alekseylapi\Lessons\Lesson7\HomeWork::printDogsInfo($master2);
+//
+//\Alekseylapi\Lessons\Lesson7\HomeWork::transferDog($dog, $master1, $master2);
+//
+//echo "\nПосле передачи:\n";
+//\Alekseylapi\Lessons\Lesson7\HomeWork::printDogsInfo($master1);
+//\Alekseylapi\Lessons\Lesson7\HomeWork::printDogsInfo($master2);
--- app/src/Lesson7/HomeWork.php
+++ app/src/Lesson7/HomeWork.php
@@ -9,29 +9,36 @@ class HomeWork
 {
     public static function buildMaster1(): Master
     {
-        $dog1 = new Dog("Шарик1", "1991-10-21", "бульдог1", "male1", "Вася1");
-        $dog2 = new Dog("Шарик2", "1991-10-22", "бульдог2", "male2", "Вася2");
-        return new Master('Вася Иванов', '1985-05-15', '+79123456789', [$dog1, $dog2]);
+        $master = new Master('Вася Иванов', '1985-05-15', '+79123456789', []);
+        $master->pets[] = new Dog("Шарик1", "1991-10-21", "бульдог1", "male1", $master);
+        $master->pets[] = new Dog("Шарик2", "1991-10-22", "бульдог2", "male2", $master);
+        return $master;
     }
 
     public static function buildMaster2(): Master
     {
-        $dog3 = new Dog("Шарик3", "1991-10-23", "бульдог3", "male3", "Вася3");
-        $dog4 = new Dog("Шарик4", "1991-10-24", "бульдог4", "male4", "Вася4");
-        $dog5 = new Dog("Шарик5", "1991-10-25", "бульдог5", "male5", "Вася5");
-        return new Master('Петя Петров', '1990-08-20', '+79876543210', [$dog3, $dog4, $dog5]);
+        $master = new Master('Петя Петров', '1990-08-20', '+79876543210', []);
+        $master->pets[] = new Dog("Шарик3", "1991-10-23", "бульдог3", "male3", $master);
+        $master->pets[] = new Dog("Шарик4", "1991-10-24", "бульдог4", "male4", $master);
+        $master->pets[] = new Dog("Шарик5", "1991-10-25", "бульдог5", "male5", $master);
+        return $master;
     }
 
 //2) Написать функцию, которая выводит информацию по всем собака хозяина(которого передали через параметр)
     public static function printDogsInfo(Master $master): void
     {
         echo "Собаки хозяина {$master->name}:" . PHP_EOL;
-        foreach ($master->pets as $dog) {
-            echo "- Имя: {$dog->name}, Порода: {$dog->breed}, Возраст: {$dog->getAge()} лет, Говорит: {$dog->say()}" . PHP_EOL;
+        foreach ($master->pets as $pet) {
+            echo "- Имя: {$pet->name}, Порода: {$pet->breed}, Возраст: {$pet->getAge()} лет, Говорит: {$pet->say()}" . PHP_EOL;
         }
     }
 
 // 3) Написать функцию, которая выводит информацию по всех хозяинам(которых передали через параметр)
+
+    /**
+     * @param Master[] $masters
+     * @return void
+     */
     public static function printMastersInfo(array $masters): void
     {
         foreach ($masters as $master) {
@@ -40,21 +47,16 @@ class HomeWork
     }
 
 //4) Написать функцию, которая выводит информацию о Хозяине(по переданной собаке)
-    public static function findMasterByDog(Dog $dog, array $masters): ?Master
+    public static function findMasterByDog(Dog $dog): void
     {
-        foreach ($masters as $master) {
-            if (in_array($dog, $master->pets, true)) {
-                return $master;
-            }
-        }
-        return null;
+     echo $dog->master->name . PHP_EOL;
     }
 
 //5) Написать функцию, которая выводит информацию о всех остальных собаках Хозяина(по переданной собаке)
-    public static function printOtherDogs(Master $master, Dog $dog): void
+    public static function printOtherDogs(Dog $dog): void
     {
-        echo "Остальные собаки хозяина {$master->name}:" . PHP_EOL;
-        foreach ($master->pets as $pet) {
+        echo "Остальные собаки хозяина {$dog->master->name}:" . PHP_EOL;
+        foreach ($dog->master->pets as $pet) {
             if ($pet !== $dog) {
                 echo "- Имя: {$pet->name}, Порода: {$pet->breed}, Возраст: {$pet->getAge()} лет" . PHP_EOL;
             }
